4101cec2024532eb780b1d7a69308a2914af6c4f
Carolyn <catalxsm@gmail.com>
2026-05-23 13:47:05 -0400

    initial setup

---
+++ hallwaysTests/hallwaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  hallwaysTests.swift
+//  hallwaysTests
+//
+//  Created by Carolyn Hua on 5/23/26.
+//
+
+import Testing
+@testable import hallways
+
+struct hallwaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
